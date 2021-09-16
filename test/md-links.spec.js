dec3378c6b0a5c994e50b041a86cbe4a64ad9e57
luvale <valeriae200@hotmail.com>
2021-09-16 16:00:01 -0500

    funcion relToAbs() close #6

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,4 @@
-const { pathExists, isAbsolute } = require('../index');
+const { pathExists, isAbsolute, relToAbs } = require('../index');
 
 describe('pathExists', () => {
   it('la ruta no existe', () => {
@@ -11,6 +11,13 @@ describe('isAbsolute', () => {
   it('la ruta no es absoluta', () => {
     const path = 'package-lock.json';
     expect(isAbsolute(path)).toBe(false);
+  });
+});
+
+describe('relToAbs', () => {
+  it('convierte la ruta a absoluta', () => {
+    const path = '.eslintrc.js';
+    expect(relToAbs(path)).toBe('/Users/luva/Laboratoria/Md Links/LIM015-md-links/.eslintrc.js');
   });
 });
 
