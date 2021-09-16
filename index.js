9935cd7538fb680676960590ac1a46c6793f61ec
luvale <valeriae200@hotmail.com>
2021-09-16 10:10:51 -0500

    funcion pathExists()

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 module.exports = () => {
   // ...
 };
+
+// File System
+const fs = require('fs');
+
+const path = '.eslintrc.js';
+const pathExists = (p) => fs.existsSync(p);
+
+console.log(pathExists(path));
