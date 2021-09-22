353508d8a16dfe522a484396c647258e97dda00b
luvale <valeriae200@hotmail.com>
2021-09-21 23:59:44 -0500

    funcion mdInDir() close #15

--- index.js
+++ index.js
@@ -12,24 +12,24 @@ const isDirectory = (p) => fs.statSync(p).isDirectory();
 
 const isMd = (p) => path.extname(p) === '.md';
 
-const practice = '/Users/luva/Laboratoria/Md Links/LIM015-md-links/prueba';
-// console.log(isMd(practice));
+const practice = '/Users/luva/Laboratoria/Md Links/LIM015-md-links/carpetaFeliz';
+// console.log(isDirectory(practice));
 
 const mdInDir = (p) => {
+  let mdArray = [];
   const readDir = fs.readdirSync(p);
-  const mdArray = [];
+  // console.log(readDir);
   for (let i = 0; i < readDir.length; i++) {
-    readDir[i] = relToAbs(readDir[i])
-    console.log(readDir[i]);
+    readDir[i] = path.join(p, '/', readDir[i])
+    // console.log(readDir[i]);
     // console.log(readDir[i], isMd(readDir[i]));
     if (isMd(readDir[i])) {
-      mdArray.push(readDir[i]);
+     mdArray.push(readDir[i]);
+    } else if(isDirectory((readDir[i]))) {
+      // console.log(mdInDir(readDir[i]));
+      mdArray = mdArray.concat(mdInDir(readDir[i]));
     }
-    /*
-    if(isDirectory((readDir[i]))) {
-      console.log(readDir[i])
-    }*/
-  } 
+  }
   return mdArray;
 };
 
