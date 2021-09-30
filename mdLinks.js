743011b1975b8e994ac5a5828d79811b5fbeb5c8
luvale <valeriae200@hotmail.com>
2021-09-30 15:13:47 -0500

    mdLinks espera la correccion de mdInDir()

--- mdLinks.js
+++ mdLinks.js
@@ -1,11 +1,27 @@
-const { pathExists, isAbsolute, relToAbs } = require('./api');
+const {
+  pathExists, isAbsolute, relToAbs, isDirectory, mdInDir, isMd,
+} = require('./api');
 
 const mdLinks = (path, options) => new Promise((resolve, reject) => {
   if (pathExists(path)) {
     const absolutePath = isAbsolute(path) ? path : relToAbs(path);
-    console.log(absolutePath);
+    const findMd = isDirectory(absolutePath) ? mdInDir(absolutePath) : absolutePath;
+    // console.log(md);
+    /* if (findMd.length !== 0) {
+      console.log(findMd);
+    } else if (isMd(findMd)) {
+      console.log('es md');
+    } else if (findMd.length === 0) {
+      console.log('Template que diga que NO hay archivos MD');
+    } */
   } else {
-    console.log('el path NO existe');
+    console.log('Template que diga que el PATH NO EXISTE');
   }
 });
-console.log(mdLinks('carpetaSinMd/túpuedes.js'));
+
+const unMd = '/Users/luva/Laboratoria/Md Links/LIM015-md-links/README.md';
+const unJs = '/Users/luva/Laboratoria/Md Links/LIM015-md-links/api.js';
+const sinMd = '/Users/luva/Laboratoria/Md Links/LIM015-md-links/carpetaSinMd';
+const conMd = '/Users/luva/Laboratoria/Md Links/LIM015-md-links/carpetaFeliz';
+
+console.log(mdLinks(unJs));
