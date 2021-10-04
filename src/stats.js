e2c6d78db3a5e2bcb82894f6dba2a2bed9e2f47f
luvale <valeriae200@hotmail.com>
2021-10-04 15:17:39 -0500

    funciones para stats y sus test

--- src/stats.js
+++ src/stats.js
@@ -1,26 +1,14 @@
-const stats = (array) => { // tiene que recibir el array que retorna getLinks()
+const linkStats = (array) => { // tiene que recibir el array que retorna getLinks()
   const totalLinks = array.length;
   const links = array.map((link) => link.href);
   const uniqueLinks = new Set(links);
-  return `Total: ${totalLinks} 
+  return `Total: ${totalLinks}
 Unique: ${uniqueLinks.size}`;
 };
 
-const arraay = [
-  {
-    href: 'https://es.wikipedia.org/wiki/Markdown',
-    text: 'Markdown',
-    file: '/Users/luva/Laboratoria/Md Links/LIM015-md-links/carpetaFeliz/prueba.md',
-  },
-  {
-    href: 'https://www.google.com/colores',
-    text: 'Colores - link roto',
-    file: '/Users/luva/Laboratoria/Md Links/LIM015-md-links/carpetaFeliz/prueba.md',
-  },
-  {
-    href: 'https://es.wikipedia.org/wiki/Markdown',
-    text: 'Markdown otra vez',
-    file: '/Users/luva/Laboratoria/Md Links/LIM015-md-links/carpetaFeliz/prueba.md',
-  },
-];
-console.log(stats(arraay));
+const brokenLinks = (array) => {
+  const linkMessages = array.filter((links) => links.message === 'fail');
+  return `Broken: ${linkMessages.length}`;
+};
+
+module.exports = { linkStats, brokenLinks };
